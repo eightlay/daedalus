ba200b9f66721625b4065bbfe2fb644c484c98c1
eightlay <timurdzhalalov@yahoo.com>
2024-03-23 10:51:31 +0100

    add run steps as goroutines example

---
+++ examples/run_steps_as_goroutines/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/eightlay/daedalus/pkg/daedalus"
+)
+
+type MyFancyStep struct {
+	iters        int
+	wait_message string
+	wait_time    int
+}
+
+func (s *MyFancyStep) Run(resolver *daedalus.Resolver) {
+	for i := 0; i < s.iters; i++ {
+		fmt.Println(s.wait_message)
+		time.Sleep(time.Duration(s.wait_time) * time.Second)
+	}
+}
+
+func (s *MyFancyStep) GetRequiredData() []string {
+	return []string{}
+}
+
+func (s *MyFancyStep) GetOutputData() []string {
+	return []string{}
+}
+
+func main() {
+	d := daedalus.NewDaedalus()
+	stage_num := d.AddStage(true)
+	d.AddStep(stage_num, &MyFancyStep{4, "step 1 waits 1 second", 1})
+	d.AddStep(stage_num, &MyFancyStep{2, "step 2 waits 2 seconds", 2})
+
+	d.Build()
+	d.Run()
+}
